198bb79c0676a44790738ff816e80703628983ab
Tobias Bernet <tobias.bernet@shopviu.com>
2020-03-02 20:18:09 +0100

    Add const fpr ranges

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+const MIN_RANGE: u32 = 1;
+const MAX_RANGE: u32 = 21;
+
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 fn main() {
     println!("Guess the number:");
 
-    let secret_number = rand::thread_rng().gen_range(1, 21);
+    let secret_number = rand::thread_rng().gen_range(MIN_RANGE, MAX_RANGE);
 
     loop {
-        println!("Please input yoour guess.");
+        println!("Please input your guess.");
 
         let mut guess = String::new(); // mutable
 

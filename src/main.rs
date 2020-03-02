7f80d282984983f175097126523db4beb1d043c5
Tobias Bernet <tobias.bernet@shopviu.com>
2020-03-02 20:06:24 +0100

    Handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use std::io;
 fn main() {
     println!("Guess the number:");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1, 21);
 
     loop {
         println!("Please input yoour guess.");
@@ -19,7 +17,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
